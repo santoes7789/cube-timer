e96a377f4fd7cad3aaf7453672f72e2931ac1c66
AJ <faronsantoso105@gmail.com>
2025-08-05 22:52:42 +1200

    circle animation when finished

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,7 +1,19 @@
 import { formatMilliseconds } from "@/utils/helpers";
 import { TimerStates } from "@/pages/Timer";
+import { useState } from "react";
+import { useEffect } from "react";
 
 const Timer = ({ time, timerState }) => {
+	const [showCircle, setShowCircle] = useState(false);
+
+	useEffect(() => {
+		if (timerState == TimerStates.STOPPED) {
+			setShowCircle(true);
+		}
+		else if (timerState == TimerStates.READY) {
+			setShowCircle(false);
+		}
+	}, [timerState])
 
 	const getTimerClasses = () => {
 		switch (timerState) {
@@ -15,8 +27,11 @@ const Timer = ({ time, timerState }) => {
 
 	return (
 		<>
-			<div className={`position-fixed vw-100 vh-100 top-0 start-0 bg-body background-timer z-2 
+			<div className={`position-fixed vw-100 vh-100 top-0 start-0 bg-body background-timer z-1 
 				${timerState == TimerStates.READY || timerState == TimerStates.RUNNING ? "show" : ""}`}></div>
+			<div className="position-fixed translate-middle">
+				{showCircle && <div className="z-2 circle"></div >}
+			</div>
 			<h1 className={`position-fixed z-3 top-50 start-50 translate-middle ${getTimerClasses()}`} id="timer">{formatMilliseconds(time)}</h1 >
 		</>
 	)
