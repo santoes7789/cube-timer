ef84afcb1700af126e4ff5195445b8c0b1c8be51
AJ <jamesa7789@gmail.com>
2026-08-09 21:27:23 +1200

    search bar for forum

--- src/pages/Forum/Forum.tsx
+++ src/pages/Forum/Forum.tsx
@@ -7,13 +7,15 @@ import { ThreadBlock } from "./ThreadBlock";
 import type { Thread } from "@/types";
 import ForumNavButtons from "./ForumNavButtons";
 import { useAuth } from "@/contexts/AuthContext";
+import { Search, X } from "lucide-react";
+import { IconButton } from "@/components/IconButton";
 
 function Forum() {
   const navigate = useNavigate();
   const [loading, setLoading] = useState(true);
   const [threads, setThreads] = useState<Thread[]>([]);
 
-  const [threadToDelete, setThreadToDelete] = useState<Thread | null>(null);
+  const [searchTerm, setSearchTerm] = useState("");
 
   const auth = useAuth();
 
@@ -28,7 +30,7 @@ function Forum() {
     <div className="forum-page-container">
       <ForumNavButtons />
       <div className="forum-heading-container">
-        <div className="popout-container" style={{ backgroundColor: "var(--bg-dark)", height: "100px", padding: '35px', display: "flex" }}>
+        <div className="popout-container" style={{ backgroundColor: "var(--bg-darker)", height: "100px", padding: '35px', display: "flex" }}>
           <div style={{ display: "flex", justifyContent: "space-between", alignItems: "flex-end", width: "100%"}}>
             <div>
               <h1>Forum</h1>
@@ -38,20 +40,34 @@ function Forum() {
           </div>
         </div>
       </div>
-      {/*<div style={{ margin: "20px 0px"}}>*/}
+
       <Divider margin={"20px"} />
-      {/*</div>*/}
 
 
-      {loading ? (
-        <div className="thread-view-container">Loading...</div>
-      ) : (
-        <div className="thread-view-container">
-          {threads.map((thread) => (
-            <ThreadBlock key={thread.id} thread={thread} isAuthor={thread.author.id === auth?.user?.id} />
-          ))}
+      <div className="thread-view-container">
+        {/*search bar*/}
+        <div className="popout-container search-bar">
+          <Search />
+          <input placeholder="Search for something..." onChange={(e) => setSearchTerm(e.target.value)} name="searchField"/>
+          { searchTerm &&
+            <IconButton icon={X} size={25} onClick={() => setSearchTerm("")} />
+          }
         </div>
-      )}
+
+        <div className="threads-container-parent">
+          {loading ? "Loading..."  : (
+            <div className="threads-container">
+              {threads.filter((item) =>
+                item.heading.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase()) ||
+                item.body.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase()) ).
+              map((thread) => (
+                <ThreadBlock key={thread.id} thread={thread} isAuthor={thread.author.id === auth?.user?.id} />
+              ))}
+            </div>
+          )}
+        </div>
+      </div>
+
       <Outlet />
     </div>
   );
